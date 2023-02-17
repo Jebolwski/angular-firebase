7ad358a08f794c5ad305ce172a786e9518550bbf
Mert Gökmen <mertgkmeen@gmail.com>
2023-02-17 22:03:53 +0300

    added navigation buttons to product-detail.component.html

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,24 +1,26 @@
-import { Component } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service';
+import {Component} from '@angular/core';
+import {ProductService} from 'src/app/services/product.service';
 import {ActivatedRoute} from "@angular/router";
-import  { SwiperOptions } from 'swiper';
+import SwiperCore, {Navigation, SwiperOptions} from 'swiper';
+
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent {
+  constructor(public productService: ProductService, private route: ActivatedRoute) {
+    SwiperCore.use([Navigation]);
+    let pid = this.route.snapshot.paramMap.get('id');
+    productService.getFavoriteProdcuts();
+    productService.getAProduct(pid || "");
+  }
 
- constructor(public productService: ProductService,private route:ActivatedRoute){
-   let pid = this.route.snapshot.paramMap.get('id');
-   productService.getFavoriteProdcuts();
-   productService.getAProduct(pid || "");
- }
-
- config: SwiperOptions = {
-   slidesPerView: 1,
-   pagination: { clickable: true },
-   scrollbar: { draggable: true },
- };
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    navigation: true,
+    pagination: {clickable: true},
+    scrollbar: {draggable: true},
+  };
 
 }
